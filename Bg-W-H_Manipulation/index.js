1dd2d0b9d54f02506b88c4ec046873c9b5d155a6
dvjnpojascodes <deviejanepojas@gmail.com>
2023-10-23 23:29:27 +0800

    Modifying Element's properties and values

---
+++ Bg-W-H_Manipulation/index.js
@@ -0,0 +1,16 @@
+var bg = document.getElementById('background');
+var width = document.getElementById('width');
+var height = document.getElementById('height');
+
+function modifyBox() {
+    var selectedBg = bg.value;
+    var selectedWidth = width.value;
+    var selectedHeight = height.value;
+    var box = document.getElementById('modify');
+
+    box.style.backgroundColor = selectedBg;
+    box.style.width = selectedWidth+"px";
+    box.style.height = selectedHeight+"px";
+}
+
+document.getElementById('set').addEventListener('click', modifyBox);
